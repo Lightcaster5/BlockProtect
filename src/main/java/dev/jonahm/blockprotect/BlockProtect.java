9f21ca4625b0f678d8a5fbf67d695723f6b26a56
Lightcaster5 <46288748+Lightcaster5@users.noreply.github.com>
2020-12-06 09:44:58 -0500

    Initial Commit

---
+++ src/main/java/dev/jonahm/blockprotect/BlockProtect.java
@@ -0,0 +1,13 @@
+package dev.jonahm.blockprotect;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockProtect extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
